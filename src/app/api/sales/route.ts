ef8f9b9aa8637287f573d60154ecfa19bb447b74
Arajtav <arajtav@gmail.com>
2024-07-17 23:18:34 +0200

    chore: move filtering to client side that's actually more optimal in bandwidth, because first request fetched all data anyway, and every other request was fetching subset of that data. also there will be less latency in the ui

--- src/app/api/sales/route.ts
+++ src/app/api/sales/route.ts
@@ -6,18 +6,8 @@ export async function GET(request: NextRequest) {
     let sales: SaleDataT[] = [
         {mcItemId: "stone",     quantity: 2*64, price: 1, shop: {seller: "Arajtav", location: {x: 0, y: 0, z: 0}}},
         {mcItemId: "calcite",   quantity: 2*64, price: 3, shop: {seller: "Arajtav", location: {x: 0, y: 0, z: 0}}},
+        {mcItemId: "mud",       quantity: 4*64, price: 5, shop: {seller: "Arajtav", location: {x: 0, y: 0, z: 0}}}
     ];
-
-    // There is no search by price, because this will be done on client side, so ui can have slider with price range
-    sales = sales.filter((s: SaleDataT) => {
-        // ts moment
-        let tmp: string | null;
-        tmp = request.nextUrl.searchParams.get("mcitem");
-        if (tmp != null && !s.mcItemId.includes(tmp.toLowerCase())) { return false; }
-        tmp = request.nextUrl.searchParams.get("seller");
-        if (tmp != null && !s.shop.seller.toLowerCase().includes(tmp.toLowerCase())) { return false; }
-        return true;
-    });
 
     return new NextResponse(JSON.stringify(sales), {
         status: 200,
