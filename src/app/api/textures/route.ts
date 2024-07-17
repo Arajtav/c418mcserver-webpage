eb10f9e6a7e4633db1b81fac2c33a9bb2b2a1ad3
Arajtav <arajtav@gmail.com>
2024-07-17 14:50:02 +0200

    feat: optimize way of displaying sold item texture

--- src/app/api/textures/route.ts
+++ src/app/api/textures/route.ts
@@ -1,17 +1,26 @@
-// TODO: THIS IS BAD, EITHER MAKE IT RETURN REAL IMAGE INSTEAD URL TO IMAGE/BASE64 ENCODED PNG
-
 import { NextRequest, NextResponse } from "next/server";
 
 const mcAssets = require("minecraft-assets")("1.20.2");
 
-export async function GET(request: NextRequest) {
-    let tmp: string | null = request.nextUrl.searchParams.get("t");
-    let texture: any | undefined = mcAssets.textureContent[tmp === null ? "missing" : tmp];
+const dataTextureMissing = Buffer.from("iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAACXBIWXMAAAsSAAALEgHS3X78AAAAEElEQVQIHWNgYGD4D4UMDAAb8gP96WVavQAAAABJRU5ErkJggg==", 'base64');
 
-    return new NextResponse(texture === undefined ? "/missing.png" : texture.texture, {
-        status: texture === undefined ? 404 : 200,
+export async function GET(request: NextRequest) {
+    let t: string | null = request.nextUrl.searchParams.get("t");
+    if (t === null || mcAssets.textureContent[t] == undefined) {
+        return new NextResponse(dataTextureMissing, {
+            status: 404,
+            headers: {
+                "Content-Type": "image/png",
+            }
+        });
+    }
+
+    let data: string = mcAssets.textureContent[t].texture.substr(22, mcAssets.textureContent[t].texture.length); // trim to only have base64 data of image
+
+    return new NextResponse(Buffer.from(data, 'base64'), {
+        status: 200,
         headers: {
-            "Content-Type": "plain",
+            "Content-Type": "image/png",
         }
     });
 }
