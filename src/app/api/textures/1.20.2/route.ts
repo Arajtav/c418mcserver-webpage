43052588cc8a9b1080af5f55a7e2a70328154cf1
Aräjtav <arajtav@arajtav.com>
2024-09-02 17:50:19 +0200

    chore: remove minecraft-assets from dependencies it also fixes some textures

--- src/app/api/textures/1.20.2/route.ts
+++ src/app/api/textures/1.20.2/route.ts
@@ -1,14 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-
-const mcAssets = require("minecraft-assets")("1.20.2");
-
-const dataTextureMissing = Buffer.from("iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAACXBIWXMAAAsSAAALEgHS3X78AAAAEElEQVQIHWNgYGD4D4UMDAAb8gP96WVavQAAAABJRU5ErkJggg==", 'base64');
+import { textures } from "./textures";
 
 export async function GET(request: NextRequest) {
     let t: string | null = request.nextUrl.searchParams.get("t");
-    let tx;
-    if (t === null || (tx = mcAssets.textureContent[t]) === undefined || tx === null || tx.texture === null) {
-        return new NextResponse(dataTextureMissing, {
+    if (t === null || textures[t] === undefined) {
+        return new NextResponse(Buffer.from(textures["missing"].texture, 'base64'), {
             status: 404,
             headers: {
                 "Content-Type": "image/png",
@@ -16,9 +12,7 @@ export async function GET(request: NextRequest) {
         });
     }
 
-    let data: string = tx.texture.substr(22, mcAssets.textureContent[t].texture.length); // trim to only have base64 data of image
-
-    return new NextResponse(Buffer.from(data, 'base64'), {
+    return new NextResponse(Buffer.from(textures[t].texture, 'base64'), {
         status: 200,
         headers: {
             "Content-Type": "image/png",
