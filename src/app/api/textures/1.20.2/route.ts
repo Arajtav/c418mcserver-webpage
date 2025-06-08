9ebb1a58d8763b15ac5e505a694d64c732a940d5
Aräjtav <arajtav@arajtav.com>
2025-06-08 19:26:44 +0200

    fix: import json for texture atlas

--- src/app/api/textures/1.20.2/route.ts
+++ src/app/api/textures/1.20.2/route.ts
@@ -1,22 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
-import { textures } from "./textures";
+import fs from "fs";
+import rawTextures from "./textures.json";
+
+const textures = new Map<string, string>(
+    Object.entries(rawTextures).map(([key, value]) => [key, value.texture]),
+);
 
 export async function GET(request: NextRequest) {
     let t: string | null = request.nextUrl.searchParams.get("t");
-    if (t === null || textures[t] === undefined) {
-        return new NextResponse(Buffer.from(textures["missing"].texture, 'base64'), {
-            status: 404,
-            headers: {
-                "Content-Type": "image/png",
-            }
+    if (t === null) {
+        return new NextResponse("T cannot be empty", {
+            status: 400,
         });
     }
+    let texture = textures.get(t);
+    if (!texture) {
+        return new NextResponse(
+            Buffer.from(textures.get("missing")!, "base64"),
+            {
+                status: 404,
+                headers: {
+                    "Content-Type": "image/png",
+                },
+            },
+        );
+    }
 
-    return new NextResponse(Buffer.from(textures[t].texture, 'base64'), {
+    return new NextResponse(Buffer.from(texture, "base64"), {
         status: 200,
         headers: {
             "Content-Type": "image/png",
             "Cache-Control": "max-age=31536000, immutable",
-        }
+        },
     });
 }
